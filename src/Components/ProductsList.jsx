ca2d4a131f59e138f47393f1abe6a42833c06338
nk-rifat <nk.rifat76@gmail.com>
2025-08-07 13:00:12 +0600

    fetch products and write function for infinite scroll inside useEffect

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,17 +1,52 @@
-import { useRef, useState } from "react";
+import {  useEffect, useRef, useState } from "react";
+
+const productsPerPage = 10;
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const loadedRef = useRef(null);
- 
+  console.log(products)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(`https://dummyjson.com/products?limit=${productsPerPage}&skip=${page * productsPerPage}`
+      );
+      const data = await response.json();
+      
+
+      if(data.products.length === 0){
+        setHasMore(false);
+      } else {
+        setProducts(prevProducts => [...prevProducts, ...data.products]);
+        setPage(prevPage => prevPage + 1);
+      }
+    };
+
+    const onIntersection = (items) => {
+      const loadedItem = items[0];
+      if (loadedItem.isIntersecting && hasMore) {
+        fetchProducts();
+      }
+    };
+    const observer = new IntersectionObserver(onIntersection);
+
+    if (observer && loadedRef.current) {
+      observer.observe(loadedRef.current);
+    }
+
+    // cleanup
+    return () => {
+      if (observer) observer.disconnect();
+    };
+  }, [hasMore, page]);
+
   return (
     <div>
       <h1>Products List</h1>
-      
 
-     <div ref={loadedRef}>Loaded More Products...</div>
+      <div ref={loadedRef}>Loaded More Products...</div>
     </div>
   );
 };
