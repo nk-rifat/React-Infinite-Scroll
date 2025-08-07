d9a8254857dc1f3570614db5282455475a92d653
nk-rifat <nk.rifat76@gmail.com>
2025-08-07 11:48:03 +0600

    project setup with React vite, Tailwind css, DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-400">React Infinite Scroll</h1>
+      <button className="btn btn-secondary">Click Me</button>
+    </>
+  );
+}
+
+export default App;
